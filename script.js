5293dcaec412d4ac2babab484592b46611e7c69e
David Alexandre <david1599@hotmail.fr>
2026-05-30 12:51:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -154,7 +154,8 @@ function animate() {
     levier2.rotateOnWorldAxis(axe_leviers_e, delta_levier2 );
     leviers_bascule.rotateOnWorldAxis(axe_bascule,  delta_bascule * 0.5 );
     piston_bascule.rotateOnWorldAxis(axe_bascule, delta_bascule * 0.0524 );
-    tige_piston_s.translateOnWorldAxis(axeZ, delta_piston_s )
+    const localAxis = axeZ.clone().transformDirection(tige_piston_s.matrixWorld.clone().invert());
+tige_piston_s.translateOnAxis(localAxis.normalize(), delta_piston_s);
 
    }
   
