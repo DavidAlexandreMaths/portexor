f8ca357c6ca5ef618e42c086fbd7bdd4ccc5ba55
David Alexandre <david1599@hotmail.fr>
2026-06-02 09:37:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ function animate() {
     if ((current_levier1 < 0) === (current_bascule < 0)) {
       target_piston_s = 0.01;
     } else {
-      target_piston_s = -0.01;
+      target_piston_s = -0.007;
     }
 
     const delta_levier1  = current_levier1  - last_levier1;  last_levier1  = current_levier1;
