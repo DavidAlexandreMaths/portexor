8281ffd10e21b341d2af2c076eaa2a0e5e47ea38
David Alexandre <david1599@hotmail.fr>
2026-06-02 16:40:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ function animate() {
     if (current_bascule < 0) {piston_bascule.rotateOnWorldAxis(axe_bascule, delta_bascule * 0.0524);}
     else {piston_bascule.rotateOnWorldAxis(axe_bascule, -delta_bascule * 0.105);}
 
-    tige_piston_bascule.translateOnAxis(axe_leviers_e, delta_bascule * 0.03);
+    tige_piston_bascule.translateOnAxis(axeZ, delta_bascule * 0.03);
 
     const localAxis = axeZ.clone().transformDirection(tige_piston_s.matrixWorld.clone().invert());
     tige_piston_s.translateOnAxis(localAxis.normalize(), delta_piston_s);
